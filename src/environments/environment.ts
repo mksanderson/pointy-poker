1df789feb543f8ba7d478fc6283e7c1eb7850b19
Matthew Sanderson <matthew@sanderson.id.au>
2025-07-12 06:52:26 +1000

    Delete src/environments directory
    Removed secret

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://nsxvvjwwgokxpkweqlrt.supabase.co', // Replace with your Supabase Project URL
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeHZ2and3Z29reHBrd2VxbHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMjU3NTcsImV4cCI6MjA2NzcwMTc1N30.b45MM4ergN4o3sz7MHM1Brt6nwugnCSW3SBqtFnaTcM' // Replace with your Supabase anon key
-};
