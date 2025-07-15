06bb9bf8999932317778bd2da5bdfd8ace96dd9c
matthewsanderson <matthew@sanderson.id.au>
2025-07-15 13:43:49 +1000

    Productionization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-// src/environments/environment.prod.ts (Production)
-// This file will be overwritten by the build script with actual environment variables
+// src/environments/environment.ts (Development)
 export const environment = {
-    production: true,
-    supabaseUrl: 'PLACEHOLDER_SUPABASE_URL',
-    supabaseKey: 'PLACEHOLDER_SUPABASE_ANON_KEY'
+    production: false,
+    supabaseUrl: 'https://nsxvvjwwgokxpkweqlrt.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeHZ2and3Z29reHBrd2VxbHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMjU3NTcsImV4cCI6MjA2NzcwMTc1N30.b45MM4ergN4o3sz7MHM1Brt6nwugnCSW3SBqtFnaTcM'
 };
